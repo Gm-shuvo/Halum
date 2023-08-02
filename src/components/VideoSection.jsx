a14ef39d3aea0aa879ef9818e2bd5177a0a1143c
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2023-08-02 22:24:38 +0600

    Adding videos section

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
+import ContenerWarrap from "./ContenerWarrap";
+import { PlayIcon } from "./PlayIcon";
+import VideoPlayerModal from "./VideoPlayerModal";
 
-const VideoSection = () => {
+const VideoSection = ({videos, loading}) => {
+  console.log('videos', videos);
+  const [isOpen, setIsOpen] = useState(false);
+  const [videoId, setVideoId] = useState(null);
   return (
-    <div>
-      <h1>video section</h1>
-    </div>
+    <ContenerWarrap>
+      {videos && (
+        <>
+          <div className="text-white text-2xl font-bold mb-4 pl-10 mt-10">
+            Offical Videos            
+          </div>
+          <div className="flex gap-10 overflow-x-auto pl-10 mt-6 mb-8">
+            {videos?.map((item) => (
+              <div key={item.id} className="">
+                <div className="relative w-64 h-36 overflow-hidden">
+                  <img
+                    src={`https://img.youtube.com/vi/${item.key}/mqdefault.jpg`}
+                    alt={item.name}
+                    className=" w-full h-full object-cover object-top rounded-md"
+                  />
+                  <div className="absolute inset-0 flex justify-center items-center">
+                    <button className="text-white text-4xl" onClick={()=> {setIsOpen(true); setVideoId(item.key)}}>
+                      <PlayIcon />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+            {isOpen && <VideoPlayerModal isOpen={isOpen} setIsOpen={setIsOpen} videoId={videoId} />}
+          </div>
+
+        </>
+      )}
+    </ContenerWarrap>
   );
 }
 
--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -3,22 +3,25 @@ import SinglePageBanner from "../components/SinglePageBanner";
 import ContenerWarrap from "../components/ContenerWarrap";
 import { useFetchData } from "../utils/RequestData";
 import Cast from "../components/Cast";
+import VideoSection from "../components/VideoSection";
 
 const SingleMovie = () => {
   const { id, type } = useParams();
 
   const {data: videos, isLoading: load1} = useFetchData(`/${type}/${id}/videos`);
   const {data: credits, isLoading: load2} = useFetchData(`/${type}/${id}/credits`);
-  console.log('videos', videos?.results?.[0]);
+  console.log('videos', videos);
   console.log('crew', credits);
   console.log('id', id);
   console.log('type', type);
 
-  
+  const trailer = videos?.results?.filter((item) => item.type === "Trailer");
+  console.log('trailer', trailer?.[0]);
   return (
     <div className="pt-16">
-      <SinglePageBanner id = {id} type={type} video= {videos?.results?.[0]} crew = {credits?.crew}/>
+      <SinglePageBanner id = {id} type={type} video= {trailer?.[0]} crew = {credits?.crew}/>
       <Cast data={credits?.cast} loading = {load2}/>
+      <VideoSection videos={videos?.results} loading = {load1}/>
 
     </div>
   );
